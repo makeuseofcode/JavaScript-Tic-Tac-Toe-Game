90bf0d877d6ea201ff7681d1b3e2c1b370b25cb4
Vondreii <Sharlene.VonDrehnen@outlook.com>
2023-01-25 17:29:39 +1100

    Updated based on changes 2 + new image

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
 let playerSymbol = "X";
 let gameEnded = false
-let winPositions = [
-	[1, 2, 3], [4, 5, 6], 
-	[7, 8, 9], [1, 4, 7], 
-	[2, 5, 8], [3, 6, 9], 
-	[1, 5, 9], [3, 5, 7]
+
+let winPos = [
+  [1, 2, 3], [4, 5, 6], 
+  [7, 8, 9], [1, 4, 7], 
+  [2, 5, 8], [3, 6, 9], 
+  [1, 5, 9], [3, 5, 7]
 ];
 
-
 for (let i = 1; i <= 9; i++) {
-	// Whenever a player clicks on a cell
-	document.getElementById(i.toString()).addEventListener(
-		"click", 
-		function() {
-			if (this.innerHTML === "" && !gameEnded) {
-				// Display either "X" or "O" in the cell, and style it
-				this.innerHTML = playerSymbol;
-				this.classList.add(playerSymbol.toLowerCase());
-				
-				// Check if a player has won
-				checkWin();
-				
-				// Swap the symbol to the other one for the next turn
-				if (playerSymbol === "X")
-					playerSymbol = "O"
-				else
-					playerSymbol = "X"
-			}
-		}
-	);
+  // Whenever a player clicks on a cell
+  document.getElementById(i.toString()).addEventListener(
+    "click", 
+    function() {
+      if (this.innerHTML === "" && !gameEnded) {
+        // Display either "X" or "O" in the cell, and style it
+        this.innerHTML = playerSymbol;
+        this.classList.add(playerSymbol.toLowerCase());
+        
+        // Check if a player has won
+        checkWin();
+        
+        // Swap the symbol to the other one for the next turn
+        if (playerSymbol === "X")
+          playerSymbol = "O"
+        else
+          playerSymbol = "X"
+      }
+    }
+  );
 }
 
 function checkWin() {
-	for (let i = 0; i < winPositions.length; i++) {
-		if (
-			document.getElementById(winPositions[i][0]).innerHTML === playerSymbol &&
-			document.getElementById(winPositions[i][1]).innerHTML === playerSymbol &&
-			document.getElementById(winPositions[i][2]).innerHTML === playerSymbol
-		) {
-			document.getElementById(winPositions[i][0]).classList.add("win");
-			document.getElementById(winPositions[i][1]).classList.add("win");
-			document.getElementById(winPositions[i][2]).classList.add("win"); 
-			gameEnded = true;
-			setTimeout(function() {
-				alert(playerSymbol + " wins!");
-			}, 500);
-		}
+  for (let i = 0; i < winPos.length; i++) {
+	if (
+	  document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
+	  document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
+	  document.getElementById(winPos[i][2]).innerHTML === playerSymbol
+	) {
+	  document.getElementById(winPos[i][0]).classList.add("win");
+	  document.getElementById(winPos[i][1]).classList.add("win");
+	  document.getElementById(winPos[i][2]).classList.add("win"); 
+	  gameEnded = true;
+	  setTimeout(function() {
+	    alert(playerSymbol + " wins!");
+	  }, 500);
 	}
+  }
 }
 
 document.getElementById("reset").addEventListener(
-    "click", 
-	function() {
-		for (let i = 1; i <= 9; i++) {
-			document.getElementById(i.toString()).innerHTML = "";
-			document.getElementById(i.toString()).classList.remove("x");
-			document.getElementById(i.toString()).classList.remove("o");
-			document.getElementById(i.toString()).classList.remove("win");
-			gameEnded = false;
-		}
-	}
+  "click", 
+  function() {
+    for (let i = 1; i <= 9; i++) {
+      document.getElementById(i.toString()).innerHTML = "";
+      document.getElementById(i.toString()).classList.remove("x");
+      document.getElementById(i.toString()).classList.remove("o");
+      document.getElementById(i.toString()).classList.remove("win");
+      gameEnded = false;
+    }
+  }
 );
